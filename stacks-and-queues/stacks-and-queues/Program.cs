354f028007649da7984e4d5a9f500b3bb9d77181
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-31 08:20:11 -0400

    bluh bluh it starts maybe

---
+++ stacks-and-queues/stacks-and-queues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace stacks_and_queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
