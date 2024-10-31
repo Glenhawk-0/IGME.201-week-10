f27b61cc133d48551874b4f954ca437714172ce8
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-31 08:47:59 -0400

    Update Program.cs

--- stacks-and-queues/stacks-and-queues/Program.cs
+++ stacks-and-queues/stacks-and-queues/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            // Print Queue
+            Queue<string> printQueue = new Queue<string>();
+
+            // Add documents to print queue
+            printQueue.Enqueue("Document1");
+            printQueue.Enqueue("Document2");
+            printQueue.Enqueue("Document3");
+
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("Options:\n- 'Print' - Print documents in printer queue.\n-'Quit' - End program.");
+
+            Console.WriteLine("Print Queue:");
+            while (printQueue.Count > 0)
+            {
+                Console.WriteLine($"Printing: {printQueue.Dequeue()}");
+            }
+
+
+
         }
     }
 }
